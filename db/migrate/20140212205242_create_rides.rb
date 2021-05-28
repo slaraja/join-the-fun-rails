e1668b7beaa84534bbaf4555dcf88e95b0dbec8d
Stephanie La Raja <stephanielaraja@gmail.com>
2021-05-27 20:15:18 -0400

    commit tree 7034f52369a5cc5b431cd1bf39b0f1f8778bd618

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,7 +1,19 @@
 class CreateRides < ActiveRecord::Migration
   def change
-    create_table :rides do |t|
+
+    create_table :passengers do |t|
+      t.timestamps
+    end
+
+    create_table :taxis do |t|
       t.timestamps null: false
     end
+
+    create_table :rides do |t|
+      t.belongs_to :passenger
+      t.belongs_to :taxi
+      t.timestamps
+    end
+    
   end
 end
